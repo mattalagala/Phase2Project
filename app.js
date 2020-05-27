04724a813a978c88dd49057e6b4a45871c553300
mattalagala <matt15y@gmail.com>
2020-05-26 19:48:35 -0500

    Added app.js, db.js, uitil.js, validate.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const db = require('./lib/db')
-const validate = require('./lib/validate')
+// const validate = require('./lib/validate')
 
 const app = express()
 
@@ -14,61 +14,23 @@ const port = 9999
 // set the template engine
 app.set('view engine', 'hbs')
 
-app.param('listUUID', function (req, res, nextFn, listUUID) {
-  db.getList(listUUID)
-    .then((theList) => {
-      req.mr_listman = req.mr_listman || {}
-      req.mr_listman.list = theList
-      nextFn()
-    })
-    .catch(() => {
-      res.status(404).send('list not found')
-    })
+app.param('listUUID', function () {
+  console.log('Called Only Once')
 })
 
-// the homepage shows your lists
+// Shows the lists on the homepage
 app.get('/', function (req, res) {
-  db.getLists()
-    .then((lists) => {
-      res.render('index', { lists: lists })
-    })
-    .catch(() => {
-      // TODO: show an error page here
-    })
+  db.getCategoryList()
+  .then((lists)=>{console.log(lists)
+    res.render('index', {lists: lists})
+  }) 
 })
 
-// the list page shows the items in the list
-app.get('/list/:listUUID', function (req, res) {
-  const theList = req.mr_listman.list
-  res.render('list_page', {
-    listUUID: theList.uuid,
-    listName: theList.name,
-    items: theList.items
+app.get('/category/:category_id', function (req, res) {
+  db.getProductsList()
+  .then((products)=> {
+    res.render('products_page', {products: products})
   })
-})
-
-// add a new item to a list
-app.post('/list/:listUUID/new-item', function (req, res) {
-  const theList = req.mr_listman.list
-  const newDescription = req.body.description
-
-  if (validate.validDescription(newDescription)) {
-    // create the item
-    db.createItem(theList.id, newDescription)
-      .then(function (newItem) {
-        res.render('item_created', {
-          listUUID: theList.uuid,
-          listName: theList.name,
-          description: newItem.description
-        })
-      })
-      .catch(() => {
-        res.status(500).send('oh man, we totally messed up')
-      })
-  } else {
-    // TODO: show them an error page
-    res.status(400).send('bad input')
-  }
 })
 
 const startExpressApp = () => {
@@ -77,13 +39,108 @@ const startExpressApp = () => {
   })
 }
 
-const bootupSequenceFailed = (err) => {
-  console.error('Unable to connect to the database:', err)
-  console.error('Goodbye!')
+function bootupSequenceFailed (err) {
+  console.error ('Uh Ohh... could not connect to the database!:', err)
+  console.error ('Goodbye!')
   process.exit(1)
 }
 
-// global kickoff point
-db.connect()
+function testSomething () {
+  db.getCategoryList()
+  .then((lists)=>{
+    // console.log ('the lists:')
+    // console.log (lists)
+  })
+}
+
+function testSomething () {
+  db.getProductsList()
+  .then((products)=>{
+    console.log (products)
+  })
+}
+
+
+// Global kickoff point
+db.connect ()
   .then(startExpressApp)
+  .then(testSomething)
+  .then(()=> {
+    console.log ('You connected to the database!')
+    })
   .catch(bootupSequenceFailed)
+  
+
+// app.param('listUUID', function (req, res, nextFn, listUUID) {
+//   db.getList(listUUID)
+//     .then((theList) => {
+//       req.inventorydb = req.inventorydb || {}
+//       req.inventorydb.list = theList
+//       nextFn()
+//     })
+//     .catch(() => {
+//       res.status(404).send('list not found')
+//     })
+// })
+
+// the homepage shows your lists
+// app.get('/', function (req, res) {
+//   db.getLists()
+//     .then((lists) => {
+//       res.render('index', { lists: lists })
+//     })
+//     .catch(() => {
+//       // TODO: show an error page here
+//     })
+// })
+
+// // the list page shows the items in the list
+// app.get('/list/:listUUID', function (req, res) {
+//   const theList = req.mr_listman.list
+//   res.render('list_page', {
+//     listUUID: theList.uuid,
+//     listName: theList.name,
+//     items: theList.items
+//   })
+// })
+
+// // add a new item to a list
+// app.post('/list/:/new-item', function (req, res) {
+//   const theList = req.mr_listman.list
+//   const newDescription = req.body.description
+
+//   if (validate.validDescription(newDescription)) {
+//     // create the item
+//     db.createItem(theList.id, newDescription)
+//       .then(function (newItem) {
+//         res.render('item_created', {
+//           listUUID: theList.uuid,
+//           listName: theList.name,
+//           description: newItem.description
+//         })
+//       })
+//       .catch(() => {
+//         res.status(500).send('oh man, we totally messed up')
+//       })
+//   } else {
+//     // TODO: show them an error page
+//     res.status(400).send('bad input')
+//   }
+// })
+
+// const startExpressApp = () => {
+//   app.listen(port, () => {
+//     console.log('express is listening on port ' + port)
+//   })
+// }
+
+// const bootupSequenceFailed = (err) => {
+//   console.error('Unable to connect to the database:', err)
+//   console.error('Goodbye!')
+//   process.exit(1)
+// }
+
+// // global kickoff point
+// db.connect()
+//   .then(startExpressApp)
+//   .catch(bootupSequenceFailed)
